9727369f04a62f0157c90b1dca4963a04c38c8cd
Emre Vatansever <e.vatansever885@mybvc.ca>
2025-12-15 20:54:06 -0700

    Final

---
+++ TechnicalArtFinal/Assets/Scripts/SimplePlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SimplePlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // devrilmesin
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y);
+        Vector3 velocity = move * speed;
+        velocity.y = rb.linearVelocity.y;
+
+        rb.linearVelocity = velocity;
+    }
+
+    // ⚠️ PlayerInput -> Send Messages bunu otomatik çağırır
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
